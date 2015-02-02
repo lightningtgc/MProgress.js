e1c540f32aa8e729c993eb1f0ffb7eae6ea0caff
lightningtgc <lightningtgc@gmail.com>
2015-02-02 21:35:55 +0800

    Add gulp taskManger,use stylus to rewrite css

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
+var paths = {
+    styl: './src/css/*.styl'
+}
+
+gulp.task('stylus', function(){
+    gulp.src(paths.styl)
+        .pipe(stylus())
+        .pipe(gulp.dest('./bulid'));
+});
+
+gulp.watch(paths.styl, ['stylus']);
+
+gulp.task('default', ['stylus']);
